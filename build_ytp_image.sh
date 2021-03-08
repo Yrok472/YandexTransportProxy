dd1531ffb99ce8435f87aee7592dd9a4931e0902
Yury Sagalaev <yury.sagalaev@acronis.com>
2021-03-09 01:09:01 +0300

    change registry

--- build_ytp_image.sh
+++ build_ytp_image.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-docker image rm yrok472/ytp
+# "cd" into "ytp" before script running
+
+# Preconditions:
+# docker login https://harbor.smartvz.site # enter creds from https://harbor.smartvz.site
+
+docker image rm infra/ytp
 docker image rm ytp
 docker build . -t ytp:latest
-docker tag ytp yrok472/ytp:latest
-docker push yrok472/ytp
+docker tag ytp harbor.smartvz.site/infra/ytp:latest
+docker push harbor.smartvz.site/infra/ytp:latest
